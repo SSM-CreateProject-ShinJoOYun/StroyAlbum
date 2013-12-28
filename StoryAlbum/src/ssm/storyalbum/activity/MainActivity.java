a0116ccd2b36311fa544e2d1550bf02d9f1b0e0d
Shin Hongseob <bluespace.dev@gmail.com>
2013-12-28 18:13:33 +0900

    commit test

--- StoryAlbum/src/ssm/storyalbum/activity/MainActivity.java
+++ StoryAlbum/src/ssm/storyalbum/activity/MainActivity.java
@@ -2,14 +2,25 @@ package ssm.storyalbum.activity;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
-
+	
+	private final String TAG = "MainActivity";
+ 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.i(TAG, "onCreate()"); 
         setContentView(R.layout.activity_main);
+    }
+    
+    @Override
+    protected void onResume() {
+    	// TODO Auto-generated method stub
+    	super.onResume();
+    	Log.i(TAG, "onResume()"); 
     }
 
 
