ea970146c264afb9ba17f43e9e097e5755dca231
Shin Hongseob <bluespace.dev@gmail.com>
2014-01-17 15:57:28 +0900

    first shelves complete

--- StoryAlbum/src/ssm/storyalbum/activity/MainActivity.java
+++ StoryAlbum/src/ssm/storyalbum/activity/MainActivity.java
@@ -1,23 +1,30 @@
 package ssm.storyalbum.activity;
 
-import android.os.Bundle;
+import ssm.storyalbum.R;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 	
 	private final String TAG = "MainActivity";
- 
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.i(TAG, "onCreate()"); 
         setContentView(R.layout.activity_main);
+         
+        Button btn_goShelves = (Button)findViewById(R.id.btn_goShelves);
+        btn_goShelves.setOnClickListener(btnClick);
     }
     
-    @Override
-    protected void onResume() {
+    @Override 
+    protected void onResume() { 
     	// TODO Auto-generated method stub
     	super.onResume();
     	Log.i(TAG, "onResume()"); 
@@ -29,5 +36,23 @@ public class MainActivity extends Activity {
     	super.onDestroy();
     	Log.i(TAG, "onDestroy() test");  
     }
+    
+    private OnClickListener btnClick = new OnClickListener(){
+
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			switch (v.getId()) {
+			case R.id.btn_goShelves:
+				Intent i = new Intent(MainActivity.this, ShelvesActivity.class);
+				startActivity(i);
+				break;
+
+			default:
+				break;
+			}
+		}
+    	
+    };
     
 }
