130435f6b2ef959bf9cd850a59cce7723dbd7185
Shin Hongseob <bluespace.dev@gmail.com>
2014-02-03 17:11:05 +0900

    actionbar frame

--- StoryAlbum/src/ssm/storyalbum/activity/MainActivity.java
+++ StoryAlbum/src/ssm/storyalbum/activity/MainActivity.java
@@ -1,6 +1,7 @@
 package ssm.storyalbum.activity;
 
 import ssm.storyalbum.R;
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -21,11 +22,13 @@ public class MainActivity extends Activity {
          
         Button btn_goShelves = (Button)findViewById(R.id.btn_goShelves);
         btn_goShelves.setOnClickListener(btnClick);
+        
+        
     }
     
     @Override 
     protected void onResume() { 
-    	// TODO Auto-generated method stub
+    	// TODO Auto-generated method stub  
     	super.onResume();
     	Log.i(TAG, "onResume()"); 
     }
